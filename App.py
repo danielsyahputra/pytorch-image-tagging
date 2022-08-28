dce0bad5fb3d415b4ab90e5a2d787b1de1a424dc
danielsyahputra13 <dnlshp@gmail.com>
2022-08-28 21:23:50 +0700

    refactor code and add some styles

--- App.py
+++ App.py
@@ -4,27 +4,45 @@ from PIL import Image
 from streamlit_tags import st_tags
 from utils import MyPredictor
 
-st.set_page_config(
-    page_title="Fashion Tagging",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+def load_settings():
+    # Page config
+    st.set_page_config(
+        page_title="Fashion Tagging",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
 
-# Title
-st.markdown('## Auto Tagging for Fashion Retail Using Multi-label Image Classification')
+    # Title
+    st.markdown('## Auto Tagging for Fashion Retail Using Multi-label Image Classification')
+    
+    # Sidebar
+    sidebar = st.sidebar
 
-uploaded_file = st.file_uploader(label="Choose a file", type=['jpg', 'jpeg'])
+    with open("assets/test.jpg", "rb") as file:
+        btn = sidebar.download_button(
+                label="Download sample image",
+                data=file,
+                file_name="test.jpg",
+                mime="image/jpg"
+            )
 
-predictor = MyPredictor()
+def main():
+    load_settings()
+    uploaded_file = st.file_uploader(label="Choose a file", type=['jpg', 'jpeg'])
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    predictor = MyPredictor()
 
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown('<p style="text-align: center;">Your Image</p><hr>', unsafe_allow_html=True)
-        st.image(image, caption="Fashion Image")
-    with col2:
-        st.markdown('<p style="text-align: center;">Image Tags</p><hr>', unsafe_allow_html=True)
-        tags = predictor.predict(image=image)
-        tags = st_tags(label="Tags:", text="Add more", value=tags)
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown('<p style="text-align: center;">Your Image</p><hr>', unsafe_allow_html=True)
+            st.image(image, caption="Fashion Image")
+        with col2:
+            st.markdown('<p style="text-align: center;">Image Tags</p><hr>', unsafe_allow_html=True)
+            tags = predictor.predict(image=image)
+            tags = st_tags(label="Tags:", text="Add more", value=tags)
+
+if __name__=="__main__":
+    main()
